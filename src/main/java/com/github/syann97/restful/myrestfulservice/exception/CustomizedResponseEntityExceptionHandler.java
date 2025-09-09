da7c6ab4ff2104fbf8f4a06d91e960b0cc09e574
syann97 <shallom75321@gmail.com>
2025-09-09 19:24:18 +0900

    feat: Exception handler using Spring AOP #7

--- src/main/java/com/github/syann97/restful/myrestfulservice/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/github/syann97/restful/myrestfulservice/exception/CustomizedResponseEntityExceptionHandler.java
@@ -13,14 +13,16 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 	@ExceptionHandler(Exception.class)
 	public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
-		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		ExceptionResponse exceptionResponse =
+			new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
 
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 	@ExceptionHandler(UserNotFoundException.class)
 	public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request) {
-		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		ExceptionResponse exceptionResponse =
+			new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
 
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
 	}
