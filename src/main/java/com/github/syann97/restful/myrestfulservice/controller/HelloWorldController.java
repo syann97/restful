c006245a1deb919510dd58e6e4cf510080e28f61
syann97 <shallom75321@gmail.com>
2025-09-09 12:51:51 +0900

    feat: add path-variable handler method #1

--- src/main/java/com/github/syann97/restful/myrestfulservice/controller/HelloWorldController.java
+++ src/main/java/com/github/syann97/restful/myrestfulservice/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.github.syann97.restful.myrestfulservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.syann97.restful.myrestfulservice.bean.HelloWorldBean;
@@ -15,5 +16,10 @@ public class HelloWorldController {
 	@GetMapping("hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World!");
+	}
+
+	@GetMapping("/hello-world-bean/path-variable/{name}")
+	public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World %s", name));
 	}
 }
