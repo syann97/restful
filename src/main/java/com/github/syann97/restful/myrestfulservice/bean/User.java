1a5cff469d5e34320783129091b3b8f5a8a1ac0c
syann97 <shallom75321@gmail.com>
2025-09-10 16:19:43 +0900

    feat: implement Validation API #11

--- src/main/java/com/github/syann97/restful/myrestfulservice/bean/User.java
+++ src/main/java/com/github/syann97/restful/myrestfulservice/bean/User.java
@@ -2,6 +2,8 @@ package com.github.syann97.restful.myrestfulservice.bean;
 
 import java.util.Date;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,6 +11,10 @@ import lombok.Data;
 @AllArgsConstructor
 public class User {
 	private Integer id;
+
+	@Size(min = 2, message = "Name은 2글자 이상 입력해주세요.")
 	private String name;
+
+	@Past(message = "등록일은 미래 날짜를 입력하실 수 없습니다.")
 	private Date joinDate;
 }
