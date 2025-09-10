8fd1a0cad06ceda1cde8c3b0fa20a0102a37ce89
syann97 <shallom75321@gmail.com>
2025-09-10 16:24:40 +0900

    feat: Create handleMethodArgumentNotValid #11

--- src/main/java/com/github/syann97/restful/myrestfulservice/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/github/syann97/restful/myrestfulservice/exception/CustomizedResponseEntityExceptionHandler.java
@@ -2,8 +2,11 @@ package com.github.syann97.restful.myrestfulservice.exception;
 
 import java.util.Date;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -25,5 +28,14 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 			new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
 
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+		HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		ExceptionResponse exceptionResponse =
+			new ExceptionResponse(new Date(), ex.getMessage(), ex.getBindingResult().toString());
+
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
 	}
 }
