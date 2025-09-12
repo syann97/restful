b810ec1bf14efb888b5ad902674bccc3444512d5
syann97 <shallom75321@gmail.com>
2025-09-12 16:15:21 +0900

    feat: Add GET method to retrieve a single user #29

--- src/main/java/com/github/syann97/restful/myrestfulservice/controller/UserJpaController.java
+++ src/main/java/com/github/syann97/restful/myrestfulservice/controller/UserJpaController.java
@@ -1,12 +1,20 @@
 package com.github.syann97.restful.myrestfulservice.controller;
 
-import java.util.List;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.syann97.restful.myrestfulservice.bean.User;
+import com.github.syann97.restful.myrestfulservice.exception.UserNotFoundException;
 import com.github.syann97.restful.myrestfulservice.repository.UserRepository;
 
 @RestController
@@ -20,8 +28,24 @@ public class UserJpaController {
 	}
 
 	@GetMapping("/users")
-	public List<User> getAllUsers() {
+	public List<User> retrieveAllUsers() {
 		return userRepository.findAll();
+	}
+
+	@GetMapping("/users/{id}")
+	public ResponseEntity<EntityModel> retrieveUserById(@PathVariable int id) {
+		Optional<User> user = userRepository.findById(id);
+
+		if (user.isEmpty()) {
+			throw new UserNotFoundException("id = " + id);
+		}
+
+		EntityModel entityModel = EntityModel.of(user.get());
+
+		WebMvcLinkBuilder linTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		entityModel.add(linTo.withRel("all-users"));
+
+		return ResponseEntity.ok(entityModel);
 	}
 
 }
